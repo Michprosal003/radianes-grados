8de605544384dc886bc764c2b522a6a42a7dad7a
Programador Michel Rosales <95501615+Michprosal003@users.noreply.github.com>
2022-03-28 16:49:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace radianes_grados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radianes, pi, resultado;
+
+            pi = 3.14159;
+
+            Console.Title = " Conversion de Radianes a Grados";
+            Console.WriteLine(" radianes");
+            radianes = double.Parse(Console.ReadLine());
+
+            resultado = (radianes * 180 / pi);
+
+            Console.WriteLine(" La respuesta es : {0} grados", resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
